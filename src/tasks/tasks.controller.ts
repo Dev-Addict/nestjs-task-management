2bc9174d26a909902784d58c2ffbb3f04df1c01f
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-03 14:00:17 +0430

    some refactor

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,44 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
-
-import { TasksService } from './tasks.service';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
-import { TaskStatus, Task } from './task.model';
+import { Controller } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService) {
-  }
-
-  @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    if (Object.keys(filterDto).length)
-      return this.tasksService.getFilteredTasks(filterDto);
-    return this.tasksService.getAllTasks();
-  }
-
-  @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
-    return this.tasksService.getTaskById(id);
-  }
-
-  @Post()
-  @UsePipes(ValidationPipe)
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(createTaskDto);
-  }
-
-  @Patch('/:id/status')
-  updateTaskStatusById(
-    @Param('id') id: string,
-    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  ): Task {
-    return this.tasksService.updateTaskStatusById(id, status);
-  }
-
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    return this.tasksService.deleteTaskById(id);
-  }
 }
