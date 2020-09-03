26cac3abb77910560db78db8edf096bebd2e182b
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-03 23:45:11 +0430

    create get user decorator

---
+++ src/auth/decorators/get-user.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator } from '@nestjs/common';
+
+import { User } from '../user.entity';
+
+export const GetUser = createParamDecorator((_, req): User => {
+  return req.user;
+});
