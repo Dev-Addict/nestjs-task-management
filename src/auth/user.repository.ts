4284d4d9ebe926a92fbf06a2d6078d9c22ae7ba7
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-03 20:02:11 +0430

    unique username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,24 +3,31 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { PublicUserDto } from './dto/public-user.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<PublicUserDto> {
   async signUp(authCredentialsDto: AuthCredentialsDto) {
-    const {username, password} = authCredentialsDto;
+    const { username, password } = authCredentialsDto;
 
     const user = new User();
 
     user.username = username;
     user.password = password;
 
-    await user.save();
+    try {
+      await user.save();
+    } catch ({code}) {
+      if (code === 23505)
+        throw new ConflictException('Username already exists');
+      throw new InternalServerErrorException();
+    }
 
     const publicUserDto = new PublicUserDto();
 
     publicUserDto.id = user.id;
     publicUserDto.username = user.username;
 
-    return publicUserDto
+    return publicUserDto;
   }
 }
